f7f22da0948d1f88a4248ce295e9a3b322cfe097
midlajc <mail.midlajc@gmail.com>
2021-11-17 12:12:42 +0530

    4 and 6 completed

---
+++ Armstrong.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class ArmstrongCalculator {
+    int length;
+    boolean isArmstrong = false;
+
+    ArmstrongCalculator(int num) {
+        length = getLength(num);
+        if(num==calcSumOfPowerOfDigits(num, length))
+            isArmstrong=true;
+    }
+
+    int getLength(int num) {
+        int i;
+        for (i = 0; num != 0; i++, num = num / 10) {
+        }
+        return i;
+    }
+
+    double calcSumOfPowerOfDigits(int num, int length) {
+        if (num != 0)
+            return Math.pow(num % 10,length) + calcSumOfPowerOfDigits(num / 10, length);
+        return 0;
+    }
+}
+
+class Armstrong {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        ArmstrongCalculator acob = new ArmstrongCalculator(Integer.parseInt(scan.nextLine()));
+        System.out.print("The Given Number is ");
+        if (acob.isArmstrong)
+            System.out.println("a Armstrong Number");
+        else
+            System.out.println("not a Armstrong Number");
+        scan.close();
+    }
+}
---
+++ ArrayOperation.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class ArrayOperation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int limit = 9, largest, secondLargest, smallest;
+        while (true) {
+            System.out.print("\nEnter limit of Array ( > 10): ");
+            limit = Integer.parseInt(scan.nextLine());
+            if (limit > 9)
+                break;
+        }
+        int numArray[] = new int[limit];
+        System.out.print("Enter Elements into Array: ");
+        for (int i = 0; i < limit; i++) {
+            numArray[i] = Integer.parseInt(scan.nextLine());
+        }
+        largest=secondLargest=smallest=numArray[0];
+        for (int i = 1; i < limit; i++) {
+            if(numArray[i]>largest)
+                largest=numArray[i];
+            if(numArray[i]<smallest)
+                smallest=numArray[i];
+            if(numArray[i]>secondLargest&&numArray[i]<largest)
+                secondLargest=numArray[i];    
+        }
+        System.out.println("Lergest: "+largest);
+        System.out.println("Second Largest: "+secondLargest);
+        System.out.println("Smallest: "+smallest);
+        scan.close();
+    }
+}
