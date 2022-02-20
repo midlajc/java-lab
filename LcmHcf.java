3b88d2385402d7355ab3a4e5eee9f715d884d86c
midlajc <mail.midlajc@gmail.com>
2022-02-20 15:03:36 +0530

    12 completed

---
+++ LcmHcf.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LcmHcf {
+    public static void main(String[] args) {
+        int num1, num2, lcm, hcf;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter two Numbers: ");
+        num1 = s.nextInt();
+        num2 = s.nextInt();
+        hcf = num1;
+
+        while (true) {
+            if ((num1 % hcf == 0) && (num2 % hcf == 0))
+                break;
+            else
+                hcf--;
+        }
+
+        lcm = (num1 * num2) / hcf;
+
+        System.out.println("HCF:" + hcf);
+        System.out.println("LCM:" + lcm);
+        s.close();
+    }
+}
