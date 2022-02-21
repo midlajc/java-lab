d865aaeca3dab1c0171987782a2a649418a269ec
midlajc <mail.midlajc@gmail.com>
2022-02-21 06:24:34 +0530

    13 completed

--- Anagram.java
+++ Anagram.java
@@ -1,29 +1,40 @@
-import java.nio.CharBuffer;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
 
-public class Anagram {
-    public static void main(String[] args) {
-        String str1, str2;
+//question 13
+class Anagram {
+
+    static boolean areAnagram(char[] str1, char[] str2) {
+        int n1 = str1.length;
+        int n2 = str2.length;
+
+        if (n1 != n2)
+            return false;
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+
+        return true;
+    }
+
+    public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter First String: ");
-        str1 = s.nextLine();
-        System.out.println("Enter Second String: ");
-        str2 = s.nextLine();
+        String str1,str2;
+        System.out.print("Enter first String:");
+        str1=s.nextLine();
+        System.out.print("Enter first String:");
+        str2=s.nextLine();
 
-        CharBuffer chArray = CharBuffer.allocate(str1.length());
-        // char[] chArray = new char[str1.length()];
-
-        if (str1.length() == str2.length()) {
-            // chArray = str1.toCharArray();
-            int flag = 0;
-            for (int i = 0; i < str2.length(); i++) {
-                // chArray.
-            }
-        } else {
-            System.out.println("string length mismatches");
-        }
-
+        if (areAnagram(str1.toCharArray(), str2.toCharArray()))
+            System.out.println("The two strings are"
+                    + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                    + " anagram of each other");
         s.close();
     }
 }
